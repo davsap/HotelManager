03254c54b2644faf66db2b213638da6254b1e013
davsap <david.parto@hotmail.com>
2018-10-14 18:41:37 +0200

    initializing apps

---
+++ smarthotelmanagerapi/src/main/java/com/smarthotelmanager/api/smarthotelmanagerapi/SmarthotelmanagerapiApplication.java
@@ -0,0 +1,12 @@
+package com.smarthotelmanager.api.smarthotelmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthotelmanagerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthotelmanagerapiApplication.class, args);
+	}
+}
